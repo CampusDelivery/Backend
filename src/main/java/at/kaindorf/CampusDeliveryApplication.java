86344f461a3ba2600c10e025d04d15a2733db8c3
Tobias Schröttner <srotoc20@htl-kaindorf.at>
2024-04-10 01:01:30 +0200

    first commit

---
+++ src/main/java/at/kaindorf/CampusDeliveryApplication.java
@@ -0,0 +1,31 @@
+package at.kaindorf;
+
+import com.github.javafaker.Faker;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class CampusDeliveryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CampusDeliveryApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> {
+            Faker faker = new Faker();
+
+
+
+        };
+    }
+
+
+}
